ed8b37758280bc83211e8f1df0b0d67cc13afcd4
Dave Worth <dave@highgroove.com>
2012-04-30 10:39:08 -0400

    umm.. please to be TDDing

---
+++ scheme_test.rb
@@ -0,0 +1,72 @@
+require 'test/unit'
+load 'scheme.rb'
+
+class TestSchemeObjectAtom < Test::Unit::TestCase
+  def test_symbol_is_atom
+    assert :symbol.atom?
+  end
+
+  def test_string_is_atom
+    assert "foo".atom?
+  end
+
+  def test_numerics_are_atoms
+    assert 1.34.atom?
+  end
+
+  def test_lists_are_not_numerics
+    assert ![].atom?
+  end
+end
+
+class TestSchemeObjectSExpr < Test::Unit::TestCase
+  def test_atoms_are_sexprs
+    assert :symbol.sexpr?
+  end
+
+  def tests_lists_are_sexprs
+    assert [].sexpr?
+  end
+end
+
+class TestSchemeArrayListPredicate < Test::Unit::TestCase
+  def test_empty_lists_are_lists
+    assert [].list?
+  end
+
+  def test_flat_arrays_of_atoms_are_lists
+    assert [:a, "string and a", 3.14].list?
+  end
+
+  def test_deeply_nested_arrays_of_atoms_are_lists
+    assert [[:a, "different"], "type", [:of, 3.14]].list?
+  end
+
+  def test_atoms_are_not_lists
+    assert_raise(NoMethodError) { :a.list? }
+  end
+end
+
+class TestSchemeDeconstruction < Test::Unit::TestCase
+  def test_car_of_atom_undefined
+    assert(NoMethodError) { :a.car }
+  end
+
+  def test_car_of_list
+    assert_equal :a, [:a, :b].car
+  end
+
+  def test_cdr_of_list
+    assert_equal [:b], [:a, :b].cdr
+  end
+
+  def test_crazy_dynamic_decomposition
+    assert_equal :d, [[], [[:b,:c,:d]]].caddaadr
+  end
+end
+
+class TestSchemeListConstruction < Test::Unit::TestCase
+  def test_cons_atom_to_list
+    assert_equal [:a, [:b, :c]], [:b, :c].cons(:a)
+  end
+end
